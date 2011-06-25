49ff2a4909844344f3b39069b174422f58f4a11c
Joe Wright <joe@joejag.com>
2011-06-25 16:59:38 +0100

    removed testing code

--- src/1to10.clj
+++ src/1to10.clj
@@ -48,8 +48,6 @@
     (map #(second %))
     ))
 
-(print (my-compress '(a a a a b c c a a d e e e e)))
-
 ; P09 (**) Pack consecutive duplicates of list elements into sublists.
 ; If a list contains repeated elements they should be placed in separate sublists.
 ; (my-pack '(a a a a b c c a a d e e e e))
