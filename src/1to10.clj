41bff4b3abd635334ab09113d40cfb64e6dc0d95
Joe Wright <joe@joejag.com>
2011-06-25 17:37:36 +0100

    partition-by is my new best friend

--- src/1to10.clj
+++ src/1to10.clj
@@ -42,34 +42,28 @@
 ; (A B C A D E)
 
 (defn my-compress [coll]
-  (->>
-    (partition 2 1 (cons " " coll))
-    (remove #(= (first %) (second %)))
-    (map #(second %))
-    ))
+  (map #(first %) (partition-by identity coll))
+  )
+
+;(print (my-compress '(a a a a b c c a a d e e e e)))
 
 ; P09 (**) Pack consecutive duplicates of list elements into sublists.
 ; If a list contains repeated elements they should be placed in separate sublists.
 ; (my-pack '(a a a a b c c a a d e e e e))
 ; ((A A A A) (B) (C C) (A A) (D) (E E E E))
 
-(defn my-pack
-    ([coll] (my-pack coll '()))
-    ([coll result]
-		(cond
-		   (= 0 (count coll)) result
-		   (= 0 (count result)) (recur (drop 1 coll) (list (first coll)))
-           (= (first coll) (first (last result))) (recur (drop 1 coll) (cons (first coll) result))
-           :else (recur (drop 1 coll) (conj (first coll) result))
-		)
-	)
-)
+(defn my-pack [coll]
+  (partition-by identity coll))
 
 ;(print (my-pack '(a a a a b c c a a d e e e e)))
 
 ; P10 (*) Run-length encoding of a list.
-; Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+; Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+; Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
 ;  (encode '(a a a a b c c a a d e e e e))
 ;  ((4 A) (1 B) (2 C) (2 A) (1 D)(4 E))
 
+(defn encode [coll]
+  (map #((juxt count first) %) (my-pack coll)))
 
+;(print (encode '(a a a a b c c a a d e e e e)))
