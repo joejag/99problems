be44d8ecb0d01f5c4fdcddb1dbe28a8d03f2eee9
Joe Wright <joe@joejag.com>
2011-06-26 12:49:33 +0100

    P12 completed

--- src/11-20.clj
+++ src/11-20.clj
@@ -4,17 +4,21 @@
 ; (encode-modified '(a a a a b c c a a d e e e e))
 ; ((4 A) B (2 C) (2 A) D (4 E))
 
-(defn encode [coll]
+(defn encode [coll]  ; copied from P10
   (map #((juxt count first) %) (partition-by identity coll)))
 
 (defn encode-modified [coll]
-  (encode coll))
+  (map #(cond (= (first %) 1) (second %) :else (identity %)) (encode coll)))
 
-(print (encode-modified '(a a a a b c c a a d e e e e)))
+;(print (encode-modified '(a a a a b c c a a d e e e e)))
 
 ;P12 (**) Decode a run-length encoded list.
 ;Given a run-length code list generated as specified in problem P11. Construct its uncompressed version.
+; (decode '((4 A) B (2 C) (2 A) D (4 E)))
+; (a a a a b c c a a d e e e e)
 
+(defn decode [coll]
+  (flatten (map #(cond (seq? %) (repeat (first %) (second %)) :else %) coll)))
 
 ;P13 (**) Run-length encoding of a list (direct solution).
 ;Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists
@@ -22,6 +26,7 @@
 ; replacing the singleton lists (1 X) by X.
 ; (encode-direct '(a a a a b c c a a d e e e e))
 ; ((4 A) B (2 C) (2 A) D (4 E))
+
 
 
 ;P14 (*) Duplicate the elements of a list.
