6b186cc0276f6de829c31fadf3e58d59d6118783
Joe Wright <joe@joejag.com>
2011-06-26 22:18:35 +0100

    added next set of problems

---
+++ src/21-30.clj
@@ -0,0 +1,8 @@
+(ns com.joejag.99problems.21-30
+  (:use clojure.test))
+
+; P21 (*) Insert an element at a given position into a list.
+; * (insert-at 'alfa '(a b c d) 2)
+;   (A ALFA B C D)
+
+(print 1)
