8e8120c537f47fc442c094e17ad37ca8a43fee33
Joe Wright <joe@joejag.com>
2011-06-26 22:38:43 +0100

    21 done

--- src/21-30.clj
+++ src/21-30.clj
@@ -2,7 +2,17 @@
   (:use clojure.test))
 
 ; P21 (*) Insert an element at a given position into a list.
-; * (insert-at 'alfa '(a b c d) 2)
+; * (my-insert-at 'alfa '(a b c d) 2)
 ;   (A ALFA B C D)
 
-(print 1)
+(defn my-insert-at [item coll pos]
+  (let [split-list (split-at (dec pos) coll)]
+  (concat (first split-list) (list item) (second split-list))))
+
+; TESTS
+
+(deftest test-my-insert-at
+  (is (= '(a "alfa" b c d) (my-insert-at "alfa" '(a b c d) 2))))
+
+
+(run-tests)
