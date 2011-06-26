7b6deb2fe1325cb0256da73a025ccc428f0cc723
Joe Wright <joe@joejag.com>
2011-06-26 22:41:29 +0100

    22 uses in built func

--- src/21-30.clj
+++ src/21-30.clj
@@ -9,10 +9,20 @@
   (let [split-list (split-at (dec pos) coll)]
   (concat (first split-list) (list item) (second split-list))))
 
+;P22 (*) Create a list containing all integers within a given range.
+;If first argument is smaller than second, produce a list in decreasing order.
+;* (my-range 4 9)
+;   (4 5 6 7 8 9)
+
+(defn my-range [start finish] (range start (inc finish)))
+
 ; TESTS
 
 (deftest test-my-insert-at
   (is (= '(a "alfa" b c d) (my-insert-at "alfa" '(a b c d) 2))))
 
+
+(deftest test-my-range
+  (is (= '(4 5 6 7 8 9)  (my-range 4 9))))
 
 (run-tests)
